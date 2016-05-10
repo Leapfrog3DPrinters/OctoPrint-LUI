1946cb4893481311871205225b4be82b0bc722b5
Pim Rutgers <pim.rutgers@gmail.com>
2016-05-10 11:23:37 +0200

    Initial Bolt stuff for updating

--- octoprint_lui/__init__.py
+++ octoprint_lui/__init__.py
@@ -74,7 +74,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
         self.update_info = [
             {
                 'identifier': 'lui',
-                'path': '/home/lily/OctoPrint-LUI',
+                'path': '/home/pi/OctoPrint-LUI',
                 'update': False,
                 'action': 'update_lui',
                 'name': "Leapfrog UI",
@@ -82,7 +82,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             },
             {
                 'identifier': 'networkmanager',
-                'path': '/home/lily/OctoPrint-NetworkManager',
+                'path': '/home/pi/OctoPrint-NetworkManager',
                 'update': False,
                 'action': 'update_networkmanager',
                 'name': 'Network Manager',
@@ -90,7 +90,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             },
             {
                 'identifier': 'flasharduino',
-                'path': '/home/lily/OctoPrint-flashArduino',
+                'path': '/home/pi/OctoPrint-flashArduino',
                 'update': False,
                 'action': 'update_flasharduino',
                 'name': 'Flash Firmware Module',
@@ -98,7 +98,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             },      
             {
                 'identifier': 'octoprint',
-                'path': '/home/lily/OctoPrint',
+                'path': '/home/pi/OctoPrint',
                 'update': False,
                 'action': 'update_octoprint',
                 'name': 'OctoPrint',
@@ -148,7 +148,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             update_lui = {
                 "action": "update_lui",
                 "name": "Update LUI",
-                "command": "cd /home/lily/OctoPrint-LUI && git pull && /home/lily/OctoPrint/venv/bin/python setup.py install",
+                "command": "cd /home/pi/OctoPrint-LUI && git pull && /home/pi/OctoPrint/venv/bin/python setup.py install",
                 "confirm": False
             }
             actions.append(update_lui)
@@ -158,7 +158,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             update_networkmanager = {
                 "action": "update_networkmanager",
                 "name": "Update NetworkManager",
-                "command": "cd /home/lily/OctoPrint-NetworkManager && git pull && /home/lily/OctoPrint/venv/bin/python setup.py install",
+                "command": "cd /home/pi/OctoPrint-NetworkManager && git pull && /home/pi/OctoPrint/venv/bin/python setup.py install",
                 "confirm": False
             }
             actions.append(update_networkmanager)
@@ -168,7 +168,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             update_octoprint = {
                 "action": "update_octoprint",
                 "name": "Update OctoPrint",
-                "command": "cd /home/lily/OctoPrint && git pull && /home/lily/OctoPrint/venv/bin/python setup.py install",
+                "command": "cd /home/pi/OctoPrint && git pull && /home/pi/OctoPrint/venv/bin/python setup.py install",
                 "confirm": False
             }
             actions.append(update_octoprint)
@@ -178,7 +178,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
             update_flasharduino = {
                 "action": "update_flasharduino",
                 "name": "Update Flash Firmware Module",
-                "command": "cd /home/lily/OctoPrint-flashArduino && git pull && /home/lily/OctoPrint/venv/bin/python setup.py install",
+                "command": "cd /home/pi/OctoPrint-flashArduino && git pull && /home/pi/OctoPrint/venv/bin/python setup.py install",
                 "confirm": False
             }
             actions.append(update_flasharduino)
@@ -255,7 +255,7 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
 
     def get_settings_defaults(self):
         return {
-            "model": "Xeed",
+            "model": "Bolt!",
             "zoffset": 0,
         }
 
