5b757c97f6e141b6b8fd587e680bdd915f4bd4ee
Pim Rutgers <pim.rutgers@gmail.com>
2016-10-21 14:58:25 +0200

    add the wipe sequence for filament load

--- octoprint_lui/__init__.py
+++ octoprint_lui/__init__.py
@@ -869,6 +869,9 @@ class LUIPlugin(octoprint.plugin.UiPlugin,
         # Still don't know if this is the best spot TODO
         if self.load_filament_timer:
             self.load_filament_timer.cancel()
+        self._printer.commands(["M605 S3"]) # mirror
+        self._printer.commands(["G1 X20"]) # wipe it
+        self._printer.commands(["M605 S0"]) # back to normal
         self._printer.home(['y', 'x'])
         self._printer.commands(["M84 S60"]) # Reset stepper disable timeout to 60sec
         self._printer.commands(["M84"]) # And disable them right away for now
