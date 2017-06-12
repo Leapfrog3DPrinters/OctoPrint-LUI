3b61d4016a1690bc584f3ae2c87d521353b809df
Geert Lens <ghlens@xs4all.nl>
2017-06-12 17:12:04 +0200

    Exit tutorial when error, added settings step

--- octoprint_lui/static/js/app/viewmodels/printerstate.js
+++ octoprint_lui/static/js/app/viewmodels/printerstate.js
@@ -551,11 +551,13 @@ $(function ()  {
                 self.errorReason(data.printerErrorReason);
                 self.erroredExtruder(data.printerErrorExtruder);
                 self.showPrinterErrorFlyout();
+                self.introView.exit();
             }
             else {
                 self.closePrinterErrorFlyout();
                 self.erroredExtruder(undefined);
                 self.errorReason(undefined);
+                self.introView.exit();
             }
 
             if(data.maintenanceMode)
