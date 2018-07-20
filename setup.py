eff58289ea3b22f665305c7c831b8e8a4de88e42
Timo <timoboon2@gmail.com>
2018-07-20 15:07:42 +0200

    update the thing already

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ plugin_name = "OctoPrint-LUI"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 
-plugin_version = "1.3.4"
+plugin_version = "1.3.5"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "update function..............."
+plugin_description = "......................update function..............."
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Pim Rutgers, Erik Heidstra, Maurits Hartman, Timo Boon"
